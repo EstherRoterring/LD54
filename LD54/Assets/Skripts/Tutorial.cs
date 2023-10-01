a977f90177554e2c0bc51130880f2e6ed5d2add8
EstherRoterring <estherroterring@gmail.com>
2023-10-01 18:09:05 +0200

    Tudorial verschwindt
    destroy in skript Tutorial

--- LD54/Assets/Skripts/Tutorial.cs
+++ LD54/Assets/Skripts/Tutorial.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    public GameObject tasteD;
+    public GameObject tasteA;
+    public GameObject tasteSpace;
+    public GameObject tasteSlide;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+        {
+            Destroy(tasteD);
+        }
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+        {
+            Destroy(tasteA);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(tasteSpace);
+        }
+        if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
+        {
+            Destroy(tasteSlide);
+        }
     }
 }
